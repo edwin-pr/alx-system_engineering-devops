d5e752adedcc5851a7763d083fd6cba9e7b96204
edwin-pr <edwingitau100@gmail.com>
2023-11-24 08:20:46 -0800

    “updates”

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# create the file 'school' in /tmp, with the content 'I love puppet'
+file {'/tmp/school':
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
+
